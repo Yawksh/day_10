2ff5e5e11e826df143e6472dad3889c254c42957
HP <yawukalfentaw@gmail.com>
2023-12-23 10:08:19 +0300

    this is is day 10 of udemy academy python project

---
+++ calculator.py
@@ -0,0 +1,22 @@
+first=float(input("entaer the first number :"))
+print("/")
+print("*")
+print('+')
+print('-')
+operator=input('select the operator :')
+second=float(input('eter the second number :'))
+def opration_performed(f_num,sec_num,operator):
+    while True:
+        if operator=='*':
+            result=f_num*sec_num
+        elif operator=='/':
+            result=f_num/sec_num
+        elif operator=='+':
+            result = f_num + sec_num
+        elif operator=='-':
+            result=f_num-sec_num
+
+
+
+opration_performed(first,second,operator)
+
