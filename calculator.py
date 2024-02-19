0ddafc7d7d6edcb017edb4c1b47063e665ff9220
Yawksh <152399244+Yawksh@users.noreply.github.com>
2024-02-19 19:57:29 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,8 +9,12 @@ print('+')
 
 print('-')
 operator=input('select the operator :')
+
 second=float(input('eter the second number :'))
+
+
 def opration_performed(f_num,sec_num,operator):
+    
     while True:
         if operator=='*':
             result=f_num*sec_num
